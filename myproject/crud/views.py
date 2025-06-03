17de68d293111f32441627dd76ac5f5b7a7e5333
taka110811 <babysteps1108@gmail.com>
2025-06-03 10:43:44 +0900

    商品詳細画面の追加

--- myproject/crud/views.py
+++ myproject/crud/views.py
@@ -3,6 +3,7 @@ from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Product
 from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
 
 class TopView(TemplateView):
     template_name = "top.html"
@@ -23,3 +24,6 @@ class ProductUpdateView(UpdateView):
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('list')
+
+class ProductDetailView(DetailView):
+    model = Product
